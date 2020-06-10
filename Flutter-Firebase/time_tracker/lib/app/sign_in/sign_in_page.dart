355165618fb92b62f1bb9e55f1abcad9a614d36b
Sadra <31988724+xsadra@users.noreply.github.com>
2020-06-10 19:36:19 +0200

    FF-TT- create a new button to Sign in with Google

--- Flutter-Firebase/time_tracker/lib/app/sign_in/sign_in_page.dart
+++ Flutter-Firebase/time_tracker/lib/app/sign_in/sign_in_page.dart
@@ -7,6 +7,48 @@ class SignInPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Time Tracker'),
       ),
+      body: _buildContent(),
+      backgroundColor: Colors.grey[200],
     );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(32.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Sign in',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          RaisedButton(
+            onPressed: _signInWithGoogle,
+            child: Text(
+              'Sign in with Google',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15.0,
+              ),
+            ),
+            color: Colors.blueAccent,
+            padding: EdgeInsets.all(12.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0)
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _signInWithGoogle() {
+    //TODO: Auth with Google
+    print('Auth with Google');
   }
 }
