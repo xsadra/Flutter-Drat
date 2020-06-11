d73fc88072332957299a3c632a320f8a80d5be6f
Sadra <31988724+xsadra@users.noreply.github.com>
2020-06-11 18:12:11 +0200

    FF-TT-Create a new class 'Auth' to use as 'Dependency Injection'

---
+++ Flutter-Firebase/time_tracker/lib/services/auth.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+
+class User {
+  User({@required this.uid});
+
+  final String uid;
+}
+
+abstract class AuthBase {
+  Future<User> currentUser();
+
+  Future<User> signInAnonymously();
+
+  Future<void> signOut();
+}
+
+class Auth {
+  final _fireBaseAuthInstance = FirebaseAuth.instance;
+
+  User _userFromFirebase(FirebaseUser firebaseUser) {
+    return firebaseUser == null ? null : User(uid: firebaseUser.uid);
+  }
+
+  Future<User> currentUser() async {
+    final currentUser = await _fireBaseAuthInstance.currentUser();
+    return _userFromFirebase(currentUser);
+  }
+
+  Future<User> signInAnonymously() async {
+    final authResult = await _fireBaseAuthInstance.signInAnonymously();
+    return _userFromFirebase(authResult.user);
+  }
+
+  Future<void> signOut() async {
+    await _fireBaseAuthInstance.signOut();
+  }
+}
