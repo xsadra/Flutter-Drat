adf012302cb3c971826bf5e24b939c22613afa72
Sadra <31988724+xsadra@users.noreply.github.com>
2020-06-11 09:17:34 +0200

    FF-TT- create a new Sign in button with logo called 'SocialSignInButton'

---
+++ Flutter-Firebase/time_tracker/lib/app/sign_in/social_sign_in_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:timetracker/widgets/buttons/custom_raised_button.dart';
+
+class SocialSignInButton extends CustomRaisedButton {
+  SocialSignInButton(
+      {String assetName,
+      String text,
+      Color color,
+      Color textColor,
+      VoidCallback onPressed})
+      : super(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(assetName),
+              Text(
+                text,
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: 15.0,
+                ),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Image.asset(assetName),
+              ),
+            ],
+          ),
+          buttonColor: color,
+          height: 48.0,
+          onPressed: onPressed,
+        );
+}
