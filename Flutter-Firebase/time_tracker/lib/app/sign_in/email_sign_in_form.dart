f46cb1acc2169bd043ebe5a1a6eedbd867853015
Sadra <31988724+xsadra@users.noreply.github.com>
2020-06-12 18:16:02 +0200

    FF-TT- Create a new Page for SignIn with Email

--- Flutter-Firebase/time_tracker/lib/app/sign_in/email_sign_in_form.dart
+++ Flutter-Firebase/time_tracker/lib/app/sign_in/email_sign_in_form.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:timetracker/widgets/buttons/form_submit_button.dart';
 
 class EmailSignInForm extends StatelessWidget {
+  List<Widget> _buildChildren() {
+    return [
+      TextField(
+        decoration:
+            InputDecoration(labelText: 'Email', hintText: 'you@email.com'),
+      ),
+      SizedBox(
+        height: 12.0,
+      ),
+      TextField(
+        decoration:
+            InputDecoration(labelText: 'Password', hintText: '********'),
+        obscureText: true,
+      ),
+      SizedBox(
+        height: 12.0,
+      ),
+      FormSubmitButton(
+        text: 'Sign in',
+        onPressed: () {},
+      ),
+      FlatButton(
+        onPressed: () {},
+        child: Text('Need an account? Register'),
+      ),
+    ];
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: _buildChildren(),
+      ),
+    );
   }
 }
