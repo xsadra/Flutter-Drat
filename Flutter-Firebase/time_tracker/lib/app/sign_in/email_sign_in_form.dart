47e62610ec09af1df31785ed9425025e662f4843
Sadra <31988724+xsadra@users.noreply.github.com>
2020-06-12 17:46:29 +0200

    FF-TT- Create a new Page 'EmailSignInPage' to UI the sign in by email

---
+++ Flutter-Firebase/time_tracker/lib/app/sign_in/email_sign_in_form.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class EmailSignInForm extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ Flutter-Firebase/time_tracker/lib/app/sign_in/email_sign_in_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:timetracker/app/sign_in/email_sign_in_form.dart';
+
+class EmailSignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign in'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          child: EmailSignInForm(),
+        ),
+      ),
+      backgroundColor: Colors.grey[200],
+    );
+  }
+}
