e7c0d3f280ed7c6dbd0806bc64c42dfa30e5e5bf
Sadra <31988724+xsadra@users.noreply.github.com>
2020-06-13 14:43:35 +0200

    FF-TT- Change the state of 'EmailSignInForm' to full state

--- Flutter-Firebase/time_tracker/lib/app/sign_in/email_sign_in_form.dart
+++ Flutter-Firebase/time_tracker/lib/app/sign_in/email_sign_in_form.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:timetracker/widgets/buttons/form_submit_button.dart';
 
-class EmailSignInForm extends StatelessWidget {
+enum EmailSignInFormType { SIGN_IN, REGISTER }
+
+class EmailSignInForm extends StatefulWidget {
+  @override
+  _EmailSignInFormState createState() => _EmailSignInFormState();
+}
+
+class _EmailSignInFormState extends State<EmailSignInForm> {
   final TextEditingController _emailController = TextEditingController();
+
   final TextEditingController _passwordController = TextEditingController();
 
   List<Widget> _buildChildren() {
