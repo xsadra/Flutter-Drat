4101dcd5d1dfb12e13d01c78834a7961db1e2bad
Sadra <31988724+xsadra@users.noreply.github.com>
2020-06-15 17:05:04 +0200

    FF-TT- Using 'PlatformAlertDialog' to show dialog when user logout in 'HomePage'

--- Flutter-Firebase/time_tracker/lib/app/home_page.dart
+++ Flutter-Firebase/time_tracker/lib/app/home_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:timetracker/services/auth.dart';
+import 'package:timetracker/widgets/platform/platform_alert_dialog.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key, @required this.auth}) : super(key: key);
 
   final AuthBase auth;
+
+  Future<void> _confirmSignOut(BuildContext context) async {
+    final confirm = await PlatformAlertDialog(
+      title: 'Logout',
+      content: 'Are you sure that you want to logout?',
+      defaultActionText: 'Logout',
+      cancelActionText: 'Cancel',
+    ).show(context);
+
+    if(confirm){
+      _signOut();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +27,7 @@ class HomePage extends StatelessWidget {
         title: Text('Home Page'),
         actions: [
           FlatButton(
-            onPressed: _signOut,
+            onPressed: () => _confirmSignOut(context),
             child: Text(
               'Logout',
               style: TextStyle(
