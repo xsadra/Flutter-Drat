3a999023c874a6feb377cc32b9251ff3321f5995
Sadra <31988724+xsadra@users.noreply.github.com>
2020-06-15 16:41:58 +0200

    FF-TT- create a class 'PlatformAlertDialog', show custom dialog

---
+++ Flutter-Firebase/time_tracker/lib/widgets/platform/platform_alert_dialog.dart
@@ -0,0 +1,82 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:timetracker/widgets/platform/platform_widget.dart';
+
+class PlatformAlertDialog extends PlatformWidget {
+  PlatformAlertDialog({
+    @required this.title,
+    @required this.content,
+    @required this.defaultActionText,
+  })  : assert(title != null),
+        assert(content != null),
+        assert(defaultActionText != null);
+
+  final String title;
+  final String content;
+  final String defaultActionText;
+
+  Future<bool> show(BuildContext context) async {
+    return Platform.isIOS
+        ? await showCupertinoDialog<bool>(
+            context: context,
+            builder: (context) => this,
+          )
+        : await showDialog<bool>(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) => this,
+          );
+  }
+
+  @override
+  Widget buildCupertinoWidget(BuildContext context) {
+    return CupertinoAlertDialog(
+      title: Text(title),
+      content: Text(content),
+      actions: _buildActions(context),
+    );
+  }
+
+  @override
+  Widget buildMaterialWidget(BuildContext context) {
+    return AlertDialog(
+      title: Text(title),
+      content: Text(content),
+      actions: _buildActions(context),
+    );
+  }
+
+  List<Widget> _buildActions(BuildContext context) {
+    return [
+      PlatformAlertDialogAction(
+        onPressed: () => Navigator.of(context).pop(),
+        child: Text(defaultActionText),
+      ),
+    ];
+  }
+}
+
+class PlatformAlertDialogAction extends PlatformWidget {
+  PlatformAlertDialogAction({this.child, this.onPressed});
+
+  final Widget child;
+  final VoidCallback onPressed;
+
+  @override
+  Widget buildCupertinoWidget(BuildContext context) {
+    return CupertinoDialogAction(
+      child: child,
+      onPressed: onPressed,
+    );
+  }
+
+  @override
+  Widget buildMaterialWidget(BuildContext context) {
+    return FlatButton(
+      child: child,
+      onPressed: onPressed,
+    );
+  }
+}
