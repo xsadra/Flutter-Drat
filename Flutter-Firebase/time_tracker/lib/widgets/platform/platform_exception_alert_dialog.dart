5984618424f8bdd4f061c456b90a5d31fa4b2892
Sadra <31988724+xsadra@users.noreply.github.com>
2020-06-16 12:49:45 +0200

    FF-TT- Refactor 'PlatformExceptionAlertDialog' to customize sign in with email errors

--- Flutter-Firebase/time_tracker/lib/widgets/platform/platform_exception_alert_dialog.dart
+++ Flutter-Firebase/time_tracker/lib/widgets/platform/platform_exception_alert_dialog.dart
@@ -8,7 +8,27 @@ class PlatformExceptionAlertDialog extends PlatformAlertDialog {
     @required PlatformException exception,
   }) : super(
           title: title,
-          content: exception.message,
+          content: _message(exception),
           defaultActionText: 'OK',
         );
+
+  static String _message(PlatformException exception) {
+    return _errors[exception.code] ?? exception.message;
+  }
+
+  static Map<String, String> _errors = {
+    'ERROR_WEAK_PASSWORD': 'Password is not strong enough.',
+    'ERROR_INVALID_EMAIL': 'The email address is invalid.',
+    'ERROR_EMAIL_ALREADY_IN_USE':
+        'Email is already in use by a different account.',
+    'ERROR_WRONG_PASSWORD': '[password] is wrong.',
+    'ERROR_USER_NOT_FOUND':
+        'There is no user corresponding to the given [email] address, or if the user has been deleted.',
+    'ERROR_USER_DISABLED':
+        'User has been disabled (for example, in the Firebase console)',
+    'ERROR_TOO_MANY_REQUESTS':
+        'There was too many attempts to sign in as this user.',
+    'ERROR_OPERATION_NOT_ALLOWED':
+        'Indicates that Email & Password accounts are not enabled.',
+  };
 }
