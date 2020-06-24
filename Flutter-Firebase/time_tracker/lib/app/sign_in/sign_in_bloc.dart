e7d96ef83ec298e06c1228d522416bc5e15c32c7
Sadra <31988724+xsadra@users.noreply.github.com>
2020-06-24 11:07:25 +0200

    FF-TT- add sign in methods into 'SignInBloc'

--- Flutter-Firebase/time_tracker/lib/app/sign_in/sign_in_bloc.dart
+++ Flutter-Firebase/time_tracker/lib/app/sign_in/sign_in_bloc.dart
@@ -1,13 +1,39 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
+import 'package:timetracker/services/auth.dart';
+
 class SignInBloc {
+  SignInBloc({@required this.auth});
+
+  final AuthBase auth;
+
   final StreamController<bool> _isLoadingController = StreamController<bool>();
 
   Stream<bool> get isLoadingStream => _isLoadingController.stream;
 
-  void setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
 
   void dispose() {
     _isLoadingController.close();
+  }
+
+  Future<User> signInAnonymously() async =>
+      await _signIn(auth.signInAnonymously);
+
+  Future<User> signInWithGoogle() async => await _signIn(auth.signInWithGoogle);
+
+  Future<User> signInWithFacebook() async =>
+      await _signIn(auth.signInWithFacebook);
+
+  Future<User> _signIn(Future<User> Function() signInMethod) async {
+    try {
+      _setIsLoading(true);
+      return await signInMethod();
+    } catch (e) {
+      rethrow;
+    } finally {
+      _setIsLoading(false);
+    }
   }
 }
