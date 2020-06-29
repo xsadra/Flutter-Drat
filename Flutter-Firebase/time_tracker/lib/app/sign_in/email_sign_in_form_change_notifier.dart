4251fcc128bd8aed29ed66ab98bac3815c162bb9
Sadra <31988724+xsadra@users.noreply.github.com>
2020-06-29 17:50:36 +0200

    FF-TT- Create a new class 'EmailSignInFormChangeNotifier' to show the form using 'EmailSignInChangeModel'

---
+++ Flutter-Firebase/time_tracker/lib/app/sign_in/email_sign_in_form_change_notifier.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:timetracker/app/sign_in/email_sign_in_change_model.dart';
+import 'package:timetracker/services/auth.dart';
+import 'package:timetracker/widgets/buttons/form_submit_button.dart';
+import 'package:timetracker/widgets/platform/platform_exception_alert_dialog.dart';
+
+class EmailSignInFormChangeNotifier extends StatefulWidget {
+  EmailSignInFormChangeNotifier({@required this.model});
+
+  final EmailSignInChangeModel model;
+
+  static Widget create(BuildContext context) {
+    final AuthBase auth = Provider.of<AuthBase>(context, listen: false);
+    return ChangeNotifierProvider<EmailSignInChangeModel>(
+      create: (context) => EmailSignInChangeModel(auth: auth),
+      child: Consumer<EmailSignInChangeModel>(
+        builder: (context, model, _) =>
+            EmailSignInFormChangeNotifier(model: model),
+      ),
+    );
+  }
+
+  @override
+  _EmailSignInFormChangeNotifierState createState() =>
+      _EmailSignInFormChangeNotifierState();
+}
+
+class _EmailSignInFormChangeNotifierState
+    extends State<EmailSignInFormChangeNotifier> {
+  final TextEditingController _emailController = TextEditingController();
+
+  final TextEditingController _passwordController = TextEditingController();
+
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
+
+  EmailSignInChangeModel get model => widget.model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: _buildChildren(),
+      ),
+    );
+  }
+
+  List<Widget> _buildChildren() {
+    return [
+      _buildEmailTextField(),
+      SizedBox(
+        height: 12.0,
+      ),
+      _buildPasswordTextField(),
+      SizedBox(
+        height: 12.0,
+      ),
+      FormSubmitButton(
+        text: model.primaryButtonText,
+        onPressed: model.canSubmit ? _submit : null,
+      ),
+      FlatButton(
+        onPressed: model.isLoading ? null : _toggleFormType,
+        child: Text(model.linkButtonText),
+      ),
+    ];
+  }
+
+  TextField _buildPasswordTextField() {
+    return TextField(
+      controller: _passwordController,
+      focusNode: _passwordFocusNode,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: '********',
+        enabled: !model.isLoading,
+        errorText: model.passwordErrorText,
+      ),
+      obscureText: true,
+      textInputAction: TextInputAction.done,
+      onChanged: model.updatePassword,
+      onEditingComplete: _submit,
+    );
+  }
+
+  TextField _buildEmailTextField() {
+    return TextField(
+      controller: _emailController,
+      focusNode: _emailFocusNode,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        hintText: 'you@email.com',
+        enabled: !model.isLoading,
+        errorText: model.emailErrorText,
+      ),
+      autocorrect: false,
+      keyboardType: TextInputType.emailAddress,
+      textInputAction: TextInputAction.next,
+      onChanged: model.updateEmail,
+      onEditingComplete: () => _emailEditingComplete(),
+    );
+  }
+
+  void _emailEditingComplete() {
+    final focusNode = model.emailValidator.isValid(model.email)
+        ? _passwordFocusNode
+        : _emailFocusNode;
+    FocusScope.of(context).requestFocus(focusNode);
+  }
+
+  void _toggleFormType() {
+    model.toggleFormType();
+    _emailController.clear();
+    _passwordController.clear();
+  }
+
+  void _submit() async {
+    try {
+      await model.submit();
+      Navigator.of(context).pop();
+    } on PlatformException catch (e) {
+      PlatformExceptionAlertDialog(
+        title: 'Sign in failed',
+        exception: e,
+      ).show(context);
+    }
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+  }
+}
