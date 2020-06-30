397c8f494b544e8f26580b4f808776de300673d7
Sadra <31988724+xsadra@users.noreply.github.com>
2020-06-30 19:30:50 +0200

    FF-TT- create a singleton service class 'FirestoreService' to communicate with firebase

---
+++ Flutter-Firebase/time_tracker/lib/services/firestore_service.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+
+class FirestoreService {
+  FirestoreService._();
+
+  static final instance = FirestoreService._();
+
+  Future<void> setData({String path, Map<String, dynamic> data}) async {
+    final reference = Firestore.instance.document(path);
+    await reference.setData(data);
+  }
+
+  Stream<List<T>> collectionStream<T>({
+    @required String path,
+    @required T builder(Map<String, dynamic> data),
+  }) {
+    final reference = Firestore.instance.collection(path);
+    final snapshots = reference.snapshots();
+    return snapshots.map(
+      (snapshot) =>
+          snapshot.documents.map((element) => builder(element.data)).toList(),
+    );
+  }
+}
