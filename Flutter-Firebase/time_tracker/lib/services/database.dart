31f6a5097dd75281b5ea0f98ac0b972558f65f3d
Sadra <31988724+xsadra@users.noreply.github.com>
2020-06-30 12:53:38 +0200

    FF-TT- add a method 'createJob' to 'FirestoreDatabase' class

--- Flutter-Firebase/time_tracker/lib/services/database.dart
+++ Flutter-Firebase/time_tracker/lib/services/database.dart
@@ -1,9 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
-abstract class DataBase {}
+abstract class DataBase {
+  Future<void> createJob(Map<String, dynamic> jobData);
+}
 
 class FirestoreDatabase implements DataBase {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
 
   final String uid;
+
+  Future<void> createJob(Map<String, dynamic> jobData) async {
+    final path = '/users/$uid/jobs/job_abc';
+    final documentReference = Firestore.instance.document(path);
+    await documentReference.setData(jobData);
+  }
 }
