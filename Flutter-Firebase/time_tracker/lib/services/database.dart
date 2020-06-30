44a082bf4322b3b07d8d72d6b3ee7366ac055c53
Sadra <31988724+xsadra@users.noreply.github.com>
2020-06-30 19:33:09 +0200

    FF-TT- use 'FirestoreService' instance to read and write data to firebase in 'Database'

--- Flutter-Firebase/time_tracker/lib/services/database.dart
+++ Flutter-Firebase/time_tracker/lib/services/database.dart
@@ -1,24 +1,27 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:timetracker/app/home/models/job.dart';
 import 'package:timetracker/services/api_path.dart';
+import 'package:timetracker/services/firestore_service.dart';
 
 abstract class DataBase {
   Future<void> createJob(Job job);
+
+  Stream<List<Job>> jobsStream();
 }
 
 class FirestoreDatabase implements DataBase {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
 
   final String uid;
+  final _service = FirestoreService.instance;
 
-  Future<void> _setData({String path, Map<String, dynamic> data}) async {
-    final reference = Firestore.instance.document(path);
-    await reference.setData(data);
-  }
-
-  Future<void> createJob(Job job) async => await _setData(
+  Future<void> createJob(Job job) async => await _service.setData(
         path: ApiPath.job(uid, 'job_abc'),
         data: job.toMap(),
+      );
+
+  Stream<List<Job>> jobsStream() => _service.collectionStream(
+        path: ApiPath.jobs(uid),
+        builder: (data) => Job.fromMap(data),
       );
 }
