dc319dd4990732a5feb2687d9ca7bcc3f857cb1e
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-01 11:47:59 +0200

    FF-TT- add Form widget with two text field and save button in appbar to 'AddJobPage'

--- Flutter-Firebase/time_tracker/lib/app/home/jobs/add_job_page.dart
+++ Flutter-Firebase/time_tracker/lib/app/home/jobs/add_job_page.dart
@@ -14,12 +14,28 @@ class AddJobPage extends StatefulWidget {
 }
 
 class _AddJobPageState extends State<AddJobPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _name;
+  int _ratePerHour;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 2.0,
         title: Text('New Job'),
+        actions: [
+          FlatButton(
+            onPressed: _submit,
+            child: Text(
+              'Save',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
       ),
       body: _buildContent(),
       backgroundColor: Colors.grey[200],
@@ -33,12 +49,53 @@ class _AddJobPageState extends State<AddJobPage> {
         child: Card(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Placeholder(
-              fallbackHeight: 200,
-            ),
+            child: _buildForm(),
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: _buildChildren(),
+      ),
+    );
+  }
+
+  List<Widget> _buildChildren() {
+    return [
+      TextFormField(
+        decoration: InputDecoration(labelText: 'Job name'),
+        validator: (value) => value.isNotEmpty ? null : 'Name can\'t be empty',
+        onSaved: (newValue) => _name = newValue,
+      ),
+      TextFormField(
+        decoration: InputDecoration(labelText: 'Rate per hour'),
+        keyboardType: TextInputType.numberWithOptions(
+          signed: false,
+          decimal: false,
+        ),
+        onSaved: (newValue) => _ratePerHour = int.parse(newValue) ?? 0,
+      ),
+    ];
+  }
+
+  void _submit() {
+    if (_validateAndSaveForm()) {
+     //Save to Database...
+    }
+  }
+
+  bool _validateAndSaveForm() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
   }
 }
