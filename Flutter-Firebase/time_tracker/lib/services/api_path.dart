32c125fa2e48e84d3671ac8b6098a36d23d77310
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-03 20:09:04 +0200

    FF-TT- add 'entry,entries' firestore path to 'ApiPath' class

--- Flutter-Firebase/time_tracker/lib/services/api_path.dart
+++ Flutter-Firebase/time_tracker/lib/services/api_path.dart
@@ -1,4 +1,10 @@
 class ApiPath {
   static String job(String uid, String jobId) => '/users/$uid/jobs/$jobId';
+
   static String jobs(String uid) => '/users/$uid/jobs';
+
+  static String entry(String uid, String entryId) =>
+      '/users/$uid/entries/$entryId';
+
+  static String entries(String uid) => '/users/$uid/entries';
 }
