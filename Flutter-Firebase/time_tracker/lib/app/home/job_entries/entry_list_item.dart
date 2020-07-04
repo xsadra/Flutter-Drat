4fe2716cb23239b687a3f83e136b5e7a9162fe52
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-04 12:57:48 +0200

    FF-TT- add all Widgets to 'EntryListItem' to represent entry Info in job entry package

--- Flutter-Firebase/time_tracker/lib/app/home/job_entries/entry_list_item.dart
+++ Flutter-Firebase/time_tracker/lib/app/home/job_entries/entry_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timetracker/app/home/job_entries/format.dart';
 import 'package:timetracker/app/home/models/entry.dart';
 import 'package:timetracker/app/home/models/job.dart';
 
@@ -29,5 +30,49 @@ class EntryListItem extends StatelessWidget {
     );
   }
 
-  _buildContents(BuildContext context) {}
+  Widget _buildContents(BuildContext context) {
+    final dayOfWeek = Format.dayOfWeek(entry.start);
+    final startDate = Format.date(entry.start);
+    final startTime = TimeOfDay.fromDateTime(entry.start).format(context);
+    final endTime = TimeOfDay.fromDateTime(entry.end).format(context);
+    final durationFormatted = Format.hours(entry.durationInHour);
+
+    final pay = job.ratePerHour * entry.durationInHour;
+    final payFormatted = Format.currency(pay);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(children: [
+          Text(
+            dayOfWeek,
+            style: TextStyle(fontSize: 18.0, color: Colors.grey),
+          ),
+          SizedBox(
+            width: 15.0,
+          ),
+          Text(startDate, style: TextStyle(fontSize: 18.0)),
+          if (job.ratePerHour > 0.0) ...[
+            Expanded(child: Container()),
+            Text(
+              payFormatted,
+              style: TextStyle(fontSize: 16.0, color: Colors.green[700]),
+            ),
+          ]
+        ]),
+        Row(children: [
+          Text('$startTime - $endTime', style: TextStyle(fontSize: 16.0)),
+          Expanded(child: Container()),
+          Text(durationFormatted, style: TextStyle(fontSize: 16.0)),
+        ]),
+        if (entry.comment.isNotEmpty)
+          Text(
+            entry.comment,
+            style: TextStyle(fontSize: 12.0),
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+          ),
+      ],
+    );
+  }
 }
