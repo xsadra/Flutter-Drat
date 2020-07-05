5c53d46061c277c95a12a61300cd82192f86ec96
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-05 14:44:20 +0200

    FF-TT- create a class 'AccountPage' in account package and move all codes related to sign out from 'JobsPage'

---
+++ Flutter-Firebase/time_tracker/lib/app/home/account/account_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:timetracker/services/auth.dart';
+import 'package:timetracker/widgets/platform/platform_alert_dialog.dart';
+
+class AccountPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Account'),
+        centerTitle: true,
+        actions: [
+          FlatButton(
+            onPressed: () => _confirmSignOut(context),
+            child: Text(
+              'Logout',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _confirmSignOut(BuildContext context) async {
+    final confirm = await PlatformAlertDialog(
+      title: 'Logout',
+      content: 'Are you sure that you want to logout?',
+      defaultActionText: 'Logout',
+      cancelActionText: 'Cancel',
+    ).show(context);
+
+    if (confirm) {
+      _signOut(context);
+    }
+  }
+
+  Future<void> _signOut(BuildContext context) async {
+    try {
+      final auth = Provider.of<AuthBase>(context, listen: false);
+      await auth.signOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
--- Flutter-Firebase/time_tracker/lib/app/home/jobs/jobs_page.dart
+++ Flutter-Firebase/time_tracker/lib/app/home/jobs/jobs_page.dart
@@ -8,25 +8,10 @@ import 'package:timetracker/app/home/jobs/edit_job_page.dart';
 import 'package:timetracker/app/home/jobs/job_list_tile.dart';
 import 'package:timetracker/app/home/jobs/list_items_builder.dart';
 import 'package:timetracker/app/home/models/job.dart';
-import 'package:timetracker/services/auth.dart';
 import 'package:timetracker/services/database.dart';
-import 'package:timetracker/widgets/platform/platform_alert_dialog.dart';
 import 'package:timetracker/widgets/platform/platform_exception_alert_dialog.dart';
 
 class JobsPage extends StatelessWidget {
-  Future<void> _confirmSignOut(BuildContext context) async {
-    final confirm = await PlatformAlertDialog(
-      title: 'Logout',
-      content: 'Are you sure that you want to logout?',
-      defaultActionText: 'Logout',
-      cancelActionText: 'Cancel',
-    ).show(context);
-
-    if (confirm) {
-      _signOut(context);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,29 +26,10 @@ class JobsPage extends StatelessWidget {
               database: Provider.of<Database>(context, listen: false),
             ),
           ),
-          FlatButton(
-            onPressed: () => _confirmSignOut(context),
-            child: Text(
-              'Logout',
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.white,
-              ),
-            ),
-          ),
         ],
       ),
       body: _buildContent(context),
     );
-  }
-
-  Future<void> _signOut(BuildContext context) async {
-    try {
-      final auth = Provider.of<AuthBase>(context, listen: false);
-      await auth.signOut();
-    } catch (e) {
-      print(e.toString());
-    }
   }
 
   Widget _buildContent(BuildContext context) {
