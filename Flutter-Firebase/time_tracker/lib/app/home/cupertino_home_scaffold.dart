6c8abb031355d32df0808b137a89cdbc2d65f088
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-05 13:24:18 +0200

    FF-TT- create a Widget class 'CoupertinoHomeScaffold' in home package to build Bottom Navigation Bar Tabs

---
+++ Flutter-Firebase/time_tracker/lib/app/home/cupertino_home_scaffold.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:timetracker/app/home/tab_item.dart';
+
+class CoupertinoHomeScaffold extends StatelessWidget {
+  const CoupertinoHomeScaffold({
+    Key key,
+    @required this.currentTab,
+    @required this.onSelectTab,
+  }) : super(key: key);
+
+  final TabItem currentTab;
+  final ValueChanged<TabItem> onSelectTab;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          _buildItem(TabItem.JOBS),
+          _buildItem(TabItem.ENTRIES),
+          _buildItem(TabItem.ACCOUNT),
+        ],
+        onTap: (index) => onSelectTab(TabItem.values[index]),
+      ),
+      tabBuilder: (context, index) => null,
+    );
+  }
+
+  BottomNavigationBarItem _buildItem(TabItem tabItem) {
+    final itemData = TabItemData.allTabs[tabItem];
+    final color = currentTab == tabItem ? Colors.indigo : Colors.grey;
+    return BottomNavigationBarItem(
+      icon: Icon(
+        itemData.icon,
+        color: color,
+      ),
+      title: Text(
+        itemData.title,
+        style: TextStyle(color: color),
+      ),
+    );
+  }
+}
