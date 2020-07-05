80179f27b424d94890ae7f1314330560a8b829e9
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-05 13:25:45 +0200

    FF-TT- create a class 'HomePage' in home package to start application having Bottom Navigation Bar tabs

---
+++ Flutter-Firebase/time_tracker/lib/app/home/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:timetracker/app/home/cupertino_home_scaffold.dart';
+import 'package:timetracker/app/home/tab_item.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TabItem _currentTab = TabItem.JOBS;
+
+  @override
+  Widget build(BuildContext context) {
+    return CoupertinoHomeScaffold(
+      currentTab: _currentTab,
+      onSelectTab: (item) {},
+    );
+  }
+}
