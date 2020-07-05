38f651c9d7301c03c1dfb6ca544c1a4aba183add
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-05 14:20:53 +0200

    FF-TT- modify 'HomePage' to represent different pages by taping on each tab

--- Flutter-Firebase/time_tracker/lib/app/home/home_page.dart
+++ Flutter-Firebase/time_tracker/lib/app/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:timetracker/app/home/cupertino_home_scaffold.dart';
+import 'package:timetracker/app/home/jobs/jobs_page.dart';
 import 'package:timetracker/app/home/tab_item.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,15 +11,24 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TabItem _currentTab = TabItem.JOBS;
 
+  Map<TabItem, WidgetBuilder> get widgetBuilders {
+    return {
+      TabItem.JOBS: (_) => JobsPage(),
+      TabItem.ENTRIES: (_) => Container(),
+      TabItem.ACCOUNT: (_) => Container(),
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return CoupertinoHomeScaffold(
       currentTab: _currentTab,
       onSelectTab: _select,
+      widgetBuilders: widgetBuilders,
     );
   }
 
   void _select(TabItem tabItem) {
-    setState(() => _currentTab= tabItem);
+    setState(() => _currentTab = tabItem);
   }
 }
