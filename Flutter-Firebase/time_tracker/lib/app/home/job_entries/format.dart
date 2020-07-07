6b4fb514dadfb35c15e27bb76f2fd16331e82edb
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-07 15:10:12 +0200

    FF-TT- modify 'hours' method in 'Format' class to floor hours

--- Flutter-Firebase/time_tracker/lib/app/home/job_entries/format.dart
+++ Flutter-Firebase/time_tracker/lib/app/home/job_entries/format.dart
@@ -2,7 +2,7 @@ import 'package:intl/intl.dart';
 
 class Format {
   static String hours(double hours) {
-    final hoursNotNegative = hours < 0.0 ? 0.0 : hours;
+    final hoursNotNegative = hours < 0.0 ? 0.0 : hours.floor();
     final formatter = NumberFormat.decimalPattern();
     final formatted = formatter.format(hoursNotNegative);
     return '${formatted}h';
