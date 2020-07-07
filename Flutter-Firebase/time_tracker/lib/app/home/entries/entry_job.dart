50a35718b7002b63d2f936a1cc776bdc810695df
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-07 11:09:52 +0200

    FF-TT- create a model class 'EntryJob' with Job and Entry fields in entries package

---
+++ Flutter-Firebase/time_tracker/lib/app/home/entries/entry_job.dart
@@ -0,0 +1,9 @@
+import 'package:timetracker/app/home/models/entry.dart';
+import 'package:timetracker/app/home/models/job.dart';
+
+class EntryJob {
+  EntryJob({this.entry, this.job});
+
+  final Entry entry;
+  final Job job;
+}
