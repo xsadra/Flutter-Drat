6e0fe70ad6e47e6bfc8a0843a5185fe3091faaa2
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-08 12:23:28 +0200

    FF-TT- modify 'Avatar' class, add border around the avatar

--- Flutter-Firebase/time_tracker/lib/widgets/avatar/avatar.dart
+++ Flutter-Firebase/time_tracker/lib/widgets/avatar/avatar.dart
@@ -3,20 +3,31 @@ import 'package:flutter/material.dart';
 class Avatar extends StatelessWidget {
   const Avatar({
     Key key,
-    this.photoUrl,
     @required this.radius,
+    this.photoUrl,
+    this.borderColor = Colors.black45,
   }) : super(key: key);
 
   final String photoUrl;
   final double radius;
+  final Color borderColor;
 
   @override
   Widget build(BuildContext context) {
-    return CircleAvatar(
-      radius: radius,
-      backgroundColor: Colors.black12,
-      backgroundImage: photoUrl != null ? NetworkImage(photoUrl) : null,
-      child: photoUrl == null ? Icon(Icons.camera_alt, size: radius) : null,
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: borderColor,
+          width: 3.0,
+        ),
+      ),
+      child: CircleAvatar(
+        radius: radius,
+        backgroundColor: Colors.black12,
+        backgroundImage: photoUrl != null ? NetworkImage(photoUrl) : null,
+        child: photoUrl == null ? Icon(Icons.camera_alt, size: radius) : null,
+      ),
     );
   }
 }
