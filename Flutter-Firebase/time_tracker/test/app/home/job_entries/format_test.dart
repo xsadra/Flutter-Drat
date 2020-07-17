9360a9d6ae3f2b564b3223523ca5fbce1dfa4b50
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-17 20:14:08 +0200

    FF-TT- TEST create a test class for 'app/home/job_entries/format.dart', 'hours' method

---
+++ Flutter-Firebase/time_tracker/test/app/home/job_entries/format_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:timetracker/app/home/job_entries/format.dart';
+
+void main(){
+group('hours', (){
+  test('positive', (){
+    expect(Format.hours(10), '10h');
+  });
+  test('zero', (){
+    expect(Format.hours(0), '0h');
+  });
+  test('negative', (){
+    expect(Format.hours(-10), '0h');
+  });
+  test('decimal', (){
+    expect(Format.hours(5.1), '5h');
+  });
+});
+}
