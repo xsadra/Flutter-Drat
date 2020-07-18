fdbc65f28543f9bcdba33a41f11a972d5819cac6
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-18 11:43:54 +0200

    FF-TT- TEST create a test class for 'app/home/models/job.dart', 'fromMap' method

---
+++ Flutter-Firebase/time_tracker/test/app/home/models/job_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:timetracker/app/home/models/job.dart';
+
+void main() {
+  group('fromMap', () {
+    test('null data', () {
+      final job = Job.fromMap(null, 'did-123');
+      expect(job, null);
+    });
+
+    test('job with all properties', () {
+      final job = Job.fromMap(
+        {'name': 'Developer', 'ratePerHour': 10},
+        'did-123',
+      );
+      expect(job.id, 'did-123');
+      expect(job.name, 'Developer');
+      expect(job.ratePerHour, 10);
+      expect(
+        job,
+        Job(id: 'did-123', name: 'Developer', ratePerHour: 10),
+      );
+    });
+  });
+}
