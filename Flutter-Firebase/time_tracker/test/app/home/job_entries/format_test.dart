bcb5ef0912a55900284094304bb1e7c9717a791c
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-18 10:57:46 +0200

    FF-TT- TEST modify test class 'app/home/job_entries/format.dart', 'date' method

--- Flutter-Firebase/time_tracker/test/app/home/job_entries/format_test.dart
+++ Flutter-Firebase/time_tracker/test/app/home/job_entries/format_test.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:timetracker/app/home/job_entries/format.dart';
 
 void main() {
@@ -14,6 +16,26 @@ void main() {
     });
     test('decimal', () {
       expect(Format.hours(5.1), '5h');
+    });
+  });
+
+  group('date - US Local', () {
+    setUp(() async {
+      Intl.defaultLocale = 'en-US';
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+    test('2020-07-17', () {
+      expect(Format.date(DateTime(2020, 7, 17)), 'Jul 17, 2020');
+    });
+  });
+
+  group('date - GB Local', () {
+    setUp(() async {
+      Intl.defaultLocale = 'en-GB';
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+    test('2020-07-17', () {
+      expect(Format.date(DateTime(2020, 7, 17)), '17 Jul 2020');
     });
   });
 }
