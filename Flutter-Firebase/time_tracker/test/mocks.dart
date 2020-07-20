44e253980c081ea3d8842736d3bcd8698a6734a7
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-20 20:10:30 +0200

    FF-TT- TEST add 'MockNavigatorObserver' class to mock 'NavigatorObserver'

--- Flutter-Firebase/time_tracker/test/mocks.dart
+++ Flutter-Firebase/time_tracker/test/mocks.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:mockito/mockito.dart';
 import 'package:timetracker/services/auth.dart';
 
 class MockAuth extends Mock implements AuthBase {}
+
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
