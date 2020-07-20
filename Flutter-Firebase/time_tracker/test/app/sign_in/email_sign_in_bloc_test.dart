b70d82b8e3b28539034713ddb82447b0d59a1a00
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-20 23:26:58 +0200

    FF-TT- TEST modify test class 'app/sign_in/email_sign_in_bloc.dart', On update email & password and submit, modelStream emits the correct events

--- Flutter-Firebase/time_tracker/test/app/sign_in/email_sign_in_bloc_test.dart
+++ Flutter-Firebase/time_tracker/test/app/sign_in/email_sign_in_bloc_test.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:timetracker/app/sign_in/email_sign_in_bloc.dart';
 import 'package:timetracker/app/sign_in/email_sign_in_model.dart';
 
@@ -19,6 +21,41 @@ void main() {
   test(
       'On update email & password and submit, modelStream emits the correct events',
       () async {
-        expect(bloc.modelStream, emits(EmailSignInModel()));
-      });
+    when(mockAuth.signInWithEmailAndPassword(
+        email: anyNamed('email'),
+        password: anyNamed(
+          'password',
+        ))).thenThrow(PlatformException(code: 'ERROR'));
+
+    expect(
+      bloc.modelStream,
+      emitsInOrder([
+        EmailSignInModel(),
+        EmailSignInModel(email: 'email@mail.com'),
+        EmailSignInModel(
+          email: 'email@mail.com',
+          password: 'password',
+        ),
+        EmailSignInModel(
+          email: 'email@mail.com',
+          password: 'password',
+          submitted: true,
+          isLoading: true,
+        ),
+        EmailSignInModel(
+          email: 'email@mail.com',
+          password: 'password',
+          submitted: true,
+          isLoading: false,
+        ),
+      ]),
+    );
+
+    bloc.updateEmail('email@mail.com');
+    bloc.updatePassword('password');
+
+    try {
+      await bloc.submit();
+    } catch (e) {}
+  });
 }
