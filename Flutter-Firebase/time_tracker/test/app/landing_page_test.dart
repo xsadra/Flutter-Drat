988044886e0119cd64a1168cee77871e85c8539b
Sadra <31988724+xsadra@users.noreply.github.com>
2020-07-20 11:33:39 +0200

    FF-TT- TEST create a test class for 'app/landing_page.dart', stream waiting, null user, non-null user

---
+++ Flutter-Firebase/time_tracker/test/app/landing_page_test.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+import 'package:timetracker/app/home/home_page.dart';
+import 'package:timetracker/app/landing_page.dart';
+import 'package:timetracker/app/sign_in/sign_in_page.dart';
+import 'package:timetracker/services/auth.dart';
+
+class MockAuth extends Mock implements AuthBase {}
+
+void main() {
+  MockAuth mockAuth;
+  StreamController<User> onAuthStateChangedController;
+
+  setUp(() {
+    mockAuth = MockAuth();
+    onAuthStateChangedController = StreamController<User>();
+  });
+
+  tearDown(() {
+    onAuthStateChangedController.close();
+  });
+
+  Future<void> pumpLandingPage(WidgetTester tester) async {
+    await tester.pumpWidget(
+      Provider<AuthBase>(
+        create: (_) => mockAuth,
+        child: MaterialApp(
+          home: LandingPage(),
+        ),
+      ),
+    );
+    await tester.pump();
+  }
+
+  void stubOnAuthStateChangeYields(Iterable<User> onAuthStateChanged) {
+    onAuthStateChangedController
+        .addStream(Stream<User>.fromIterable(onAuthStateChanged));
+    when(mockAuth.onAuthStateChanged)
+        .thenAnswer((_) => onAuthStateChangedController.stream);
+  }
+
+  testWidgets('stream waiting', (WidgetTester tester) async {
+    stubOnAuthStateChangeYields([]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('null user', (WidgetTester tester) async {
+    stubOnAuthStateChangeYields([null]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(SignInPage), findsOneWidget);
+  });
+
+  testWidgets('non-null user', (WidgetTester tester) async {
+    stubOnAuthStateChangeYields([
+      User(
+        uid: 'uid-123',
+        photoUrl: null,
+        displayName: null,
+        email: null,
+        phoneNumber: null,
+      )
+    ]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(HomePage), findsOneWidget);
+  });
+}
